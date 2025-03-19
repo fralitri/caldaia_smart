a63b28219522787232824bc9161edc41e9bc0f17
Francesco Litrico <75505583+fralitri@users.noreply.github.com>
2025-03-19 22:10:53 +0100

    Update config_flow.py

--- custom_components/caldaia_smart/config_flow.py
+++ custom_components/caldaia_smart/config_flow.py
@@ -12,19 +12,32 @@ class CaldaiaSmartConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         if user_input is not None:
             return self.async_create_entry(title=user_input["device_name"], data=user_input)
 
+        sensors = self.hass.states.async_entity_ids("sensor")
+        temperature_sensors = [s for s in sensors if "temperature" in s]
+        power_sensors = [s for s in sensors if "power" in s]
+
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
                 vol.Required("device_name"): str,
-                vol.Required("acs_temp"): str,
-                vol.Required("afs_temp"): str,
-                vol.Required("mandata_temp"): str,
-                vol.Required("ritorno_temp"): str,
-                vol.Required("fumi_temp"): str,
-                vol.Required("consumo_caldaia"): str,
+                vol.Required("acs_temp"): selector.SelectSelector(
+                    selector.SelectSelectorConfig(options=temperature_sensors)
+                ),
+                vol.Required("afs_temp"): selector.SelectSelector(
+                    selector.SelectSelectorConfig(options=temperature_sensors)
+                ),
+                vol.Required("mandata_temp"): selector.SelectSelector(
+                    selector.SelectSelectorConfig(options=temperature_sensors)
+                ),
+                vol.Required("ritorno_temp"): selector.SelectSelector(
+                    selector.SelectSelectorConfig(options=temperature_sensors)
+                ),
+                vol.Required("fumi_temp"): selector.SelectSelector(
+                    selector.SelectSelectorConfig(options=temperature_sensors)
+                ),
+                vol.Required("consumo_caldaia"): selector.SelectSelector(
+                    selector.SelectSelectorConfig(options=power_sensors)
+                ),
             }),
             errors=errors,
         )
-
-    async def async_step_import(self, import_data):
-        return await self.async_step_user(import_data)
