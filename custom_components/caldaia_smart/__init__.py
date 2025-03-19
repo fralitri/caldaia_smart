02c51282fc3a819c4425035837b97a0100a44017
Francesco Litrico <75505583+fralitri@users.noreply.github.com>
2025-03-19 22:10:27 +0100

    Update __init__.py

--- custom_components/caldaia_smart/__init__.py
+++ custom_components/caldaia_smart/__init__.py
@@ -1,16 +1,6 @@
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
-from .const import DOMAIN
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    if DOMAIN not in config:
-        return True
-
-    hass.async_create_task(
-        hass.config_entries.flow.async_init(
-            DOMAIN,
-            context={"source": "configuration.yaml"},
-            data=config[DOMAIN],
-        )
-    )
+    """Set up the Caldaia Smart integration."""
     return True
