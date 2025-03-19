a30f5cbf4c8074247c433e89dc44f14efc93541a
Francesco Litrico <75505583+fralitri@users.noreply.github.com>
2025-03-19 21:59:38 +0100

    Update config_flow.py

--- custom_components/caldaia_smart/config_flow.py
+++ custom_components/caldaia_smart/config_flow.py
@@ -1,22 +1,44 @@
 import voluptuous as vol
 from homeassistant import config_entries
-from . import DOMAIN
+from homeassistant.core import callback
+from homeassistant.helpers import selector
+from .const import DOMAIN
 
 class CaldaiaSmartConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for Caldaia Smart."""
+    VERSION = 1
+    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
 
     async def async_step_user(self, user_input=None):
-        """Handle the initial step."""
+        errors = {}
         if user_input is not None:
-            return self.async_create_entry(title="Caldaia Smart", data=user_input)
+            return self.async_create_entry(title=user_input["device_name"], data=user_input)
+
+        sensors = self.hass.states.async_entity_ids("sensor")
+        temperature_sensors = [s for s in sensors if "temperature" in s]
+        power_sensors = [s for s in sensors if "power" in s]
 
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
-                vol.Required("power_sensor"): str,
-                vol.Required("standby_threshold", default=20): int,
-                vol.Required("acs_threshold", default=60): int,
-                vol.Required("circolatore_threshold", default=100): int,
-                vol.Required("riscaldamento_threshold", default=140): int
-            })
+                vol.Required("device_name"): str,
+                vol.Required("acs_temp"): selector.SelectSelector(
+                    selector.SelectSelectorConfig(options=temperature_sensors)
+                ),
+                vol.Required("afs_temp"): selector.SelectSelector(
+                    selector.SelectSelectorConfig(options=temperature_sensors)
+                ),
+                vol.Required("mandata_temp"): selector.SelectSelector(
+                    selector.SelectSelectorConfig(options=temperature_sensors)
+                ),
+                vol.Required("ritorno_temp"): selector.SelectSelector(
+                    selector.SelectSelectorConfig(options=temperature_sensors)
+                ),
+                vol.Required("fumi_temp"): selector.SelectSelector(
+                    selector.SelectSelectorConfig(options=temperature_sensors)
+                ),
+                vol.Required("consumo_caldaia"): selector.SelectSelector(
+                    selector.SelectSelectorConfig(options=power_sensors)
+                ),
+            }),
+            errors=errors,
         )
