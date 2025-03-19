1027933977ea0ec6062bdbccdd5689d203a88a84
Francesco Litrico <75505583+fralitri@users.noreply.github.com>
2025-03-19 22:26:46 +0100

    Update __init__.py

--- custom_components/caldaia_smart/__init__.py
+++ custom_components/caldaia_smart/__init__.py
@@ -1,6 +1,11 @@
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
+from .sensor import async_setup_entry
 
 async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
     """Set up the Caldaia Smart integration."""
     return True
+
+async def async_setup_entry(hass, config_entry):
+    """Set up the integration from a config entry."""
+    return await async_setup_entry(hass, config_entry)
