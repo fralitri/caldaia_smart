15f8a08f4669efde23cd26b06a1271110c620db6
Francesco Litrico <75505583+fralitri@users.noreply.github.com>
2025-03-20 22:26:02 +0100

    Update const.py

--- custom_components/caldaia_smart/const.py
+++ custom_components/caldaia_smart/const.py
@@ -1,2 +1,8 @@
 DOMAIN = "caldaia_smart"
-PLATFORMS = ["sensor"]
+CONF_NAME = "name"
+CONF_TEMP_ACS = "temp_acs"
+CONF_TEMP_ACF = "temp_acf"
+CONF_TEMP_MANDATA = "temp_mandata"
+CONF_TEMP_RITORNO = "temp_ritorno"
+CONF_TEMP_FUMI = "temp_fumi"
+CONF_CONSUMO_ELETTRICO = "consumo_elettrico"
