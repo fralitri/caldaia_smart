14ffd26ec42f82f6d3d8f3afd9a99a5c3b769e69
Francesco Litrico <75505583+fralitri@users.noreply.github.com>
2025-03-22 22:01:19 +0100

    Update __init__.py

--- custom_components/caldaia_smart/__init__.py
+++ custom_components/caldaia_smart/__init__.py
@@ -3,8 +3,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
 from homeassistant import config_entries
 from homeassistant.const import Platform
-from .const import DOMAIN, CONF_CONSUMO_ELETTRICO, CONF_STANDBY_THRESHOLD, CONF_ACS_THRESHOLD, CONF_CIRCOLATORE_THRESHOLD, CONF_RISCALDAMENTO_THRESHOLD
-from .sensor import CaldaiaSmartStatoSensor
+from .const import DOMAIN
 
 # Configura il logger
 _LOGGER = logging.getLogger(__name__)
@@ -22,28 +21,8 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: config_entries.Co
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][config_entry.entry_id] = config_entry.data
 
-    # Crea l'entità Stato Caldaia
-    consumo_elettrico = config_entry.data[CONF_CONSUMO_ELETTRICO]
-    standby_threshold = config_entry.data.get(CONF_STANDBY_THRESHOLD, 20.0)
-    acs_threshold = config_entry.data.get(CONF_ACS_THRESHOLD, 60.0)
-    circolatore_threshold = config_entry.data.get(CONF_CIRCOLATORE_THRESHOLD, 85.0)
-    riscaldamento_threshold = config_entry.data.get(CONF_RISCALDAMENTO_THRESHOLD, 130.0)
-
-    stato_sensor = CaldaiaSmartStatoSensor(
-        hass,
-        consumo_elettrico,
-        standby_threshold,
-        acs_threshold,
-        circolatore_threshold,
-        riscaldamento_threshold,
-        config_entry.entry_id
-    )
-
-    # Aggiungi l'entità a Home Assistant
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(config_entry, "sensor")
-    )
-
+    # Carica le piattaforme supportate
+    await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
     return True
 
 async def async_unload_entry(hass: HomeAssistant, config_entry: config_entries.ConfigEntry) -> bool:
