9654a1aafe37f6aaf4157b5437c37ed562fc00fb
Francesco Litrico <75505583+fralitri@users.noreply.github.com>
2025-03-23 16:51:28 +0100

    Update __init__.py

--- custom_components/caldaia_smart/__init__.py
+++ custom_components/caldaia_smart/__init__.py
@@ -21,6 +21,16 @@ async def async_setup_entry(hass: HomeAssistant, config_entry: config_entries.Co
     hass.data.setdefault(DOMAIN, {})
     hass.data[DOMAIN][config_entry.entry_id] = config_entry.data
 
+    # Crea il dispositivo principale
+    device_registry = hass.helpers.device_registry.async_get(hass)
+    device_registry.async_get_or_create(
+        config_entry_id=config_entry.entry_id,
+        identifiers={(DOMAIN, config_entry.entry_id)},
+        name=config_entry.data[CONF_NAME],  # Usa il nome inserito dall'utente
+        manufacturer="Caldaia Smart",
+        model="Generic",
+    )
+
     # Carica le piattaforme supportate
     await hass.config_entries.async_forward_entry_setups(config_entry, PLATFORMS)
     return True
